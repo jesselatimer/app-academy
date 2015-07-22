2f34533f9e25164dfd51066c1c0309579a4f9226
Jesse Latimer <jesse.latimer@gmail.com>
2015-07-22 10:09:18 -0700

    complete

--- w5d2-3/skeleton/lib/phase4/session.rb
+++ w5d2-3/skeleton/lib/phase4/session.rb
@@ -6,17 +6,24 @@ module Phase4
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      cookies = req.cookies.select { |c| c.name == "_rails_lite_app" }
+      cookie = JSON.parse(cookies.first.value) unless cookies.empty?
+      @content = cookie || {}
     end
 
     def [](key)
+      @content[key]
     end
 
     def []=(key, val)
+      @content[key] = val
     end
 
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      cookie = WEBrick::Cookie.new("_rails_lite_app", @content.to_json)
+      res.cookies << cookie
     end
   end
 end
--- w5d2-3/skeleton/lib/phase5/params.rb
+++ w5d2-3/skeleton/lib/phase5/params.rb
@@ -1,4 +1,5 @@
 require 'uri'
+require 'byebug'
 
 module Phase5
   class Params
@@ -10,9 +11,13 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = parse_www_encoded_form(req.query_string)
+      @params.merge!(parse_www_encoded_form(req.body))
+      @params.merge!(route_params)
     end
 
     def [](key)
+      @params[key.to_s] || @params[key.to_sym]
     end
 
     # this will be useful if we want to `puts params` in the server log
@@ -29,11 +34,33 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      params_hash = {}
+      return params_hash unless www_encoded_form
+
+      array = URI.decode_www_form(www_encoded_form)
+      array.each do |key_string, value|
+        keys = parse_key(key_string)
+        deep_merge!(params_hash, construct_hash(keys, value))
+      end
+
+      params_hash
+    end
+
+    def deep_merge!(hash1, hash2)
+      hash1.merge!(hash2) do |k, v1, v2|
+        v1.is_a?(Hash) && v2.is_a?(Hash) ? deep_merge!(v1, v2) : v2
+      end
+    end
+
+    def construct_hash(keys, value)
+      return { keys.first => value } if keys.length == 1
+      { keys.first => construct_hash(keys.drop(1), value) }
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      key.split(/\]\[|\[|\]/)
     end
   end
 end
