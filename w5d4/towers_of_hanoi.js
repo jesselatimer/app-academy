15b028807ed00be3da8e20149b54183f0e8c664e
Jesse Latimer <jesse.latimer@gmail.com>
2015-07-23 19:19:30 -0700

    Delete towers_of_hanoi.js

--- w5d4/towers_of_hanoi.js
+++
@@ -1,9 +0,0 @@
-var stacks = [[1, 2, 3], [], []];
-var win = false;
-
-while (!win) {
-  console.log(stacks);
-  var fromStack = prompt("Which stack to move from?");
-  var toStack = prompt("Which stack to move to?");
-  
-};
